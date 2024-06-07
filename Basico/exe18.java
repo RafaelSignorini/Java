a000513a62b65c9a8d51aeb9d72f5a3367f3034e
Senhor_Ini <rafabs26126@gmail.com>
2024-06-07 18:21:49 -0300

    Update exe18.java

--- Basico/exe18.java
+++ Basico/exe18.java
@@ -1,5 +1,17 @@
 package Basico;
 
+import java.util.Scanner;
+
 public class exe18 {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insira um ano para saber se é bissexto ou normal: ");
+        int year = scanner.nextInt();
+
+        if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) {
+            System.out.println("O ano é bissexto.");
+        } else {
+            System.out.println("O ano não é bissexto.");
+        }
+    }
 }
