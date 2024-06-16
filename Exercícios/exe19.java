cc9be3cdc7e9c8e10b63fd36f783ae91d6b5262c
Senhor_Ini <rafabs26126@gmail.com>
2024-06-15 22:54:40 -0300

    Update exe19.java

--- Exercícios/exe19.java
+++ Exercícios/exe19.java
@@ -1,9 +1,24 @@
 package Exercícios;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class exe19 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        System.out.println("Nome do primeiro aluno: ");
+        String aluno1 = scanner.nextLine();
+        System.out.println("Nome do segundo aluno: ");
+        String aluno2 = scanner.nextLine();
+        System.out.println("Nome do terceiro aluno: ");
+        String aluno3 = scanner.nextLine();
+        System.out.println("Nome do quarto aluno: ");
+        String aluno4 = scanner.nextLine();
+        String[] lista = {aluno1, aluno2, aluno3, aluno4};
+        int index = random.nextInt(lista.length);
+        String escolhido = lista[index];
+        System.out.printf("O aluno escolhido foi %s.", escolhido);
+        scanner.close();
     }
 }
