8d424406def3e3f2ef288c48ad75bffe7f3c9d3e
Senhor_Ini <rafabs26126@gmail.com>
2024-06-15 22:08:59 -0300

    Update exe16.java

--- Exercícios/exe16.java
+++ Exercícios/exe16.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 public class exe16 {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insira um valor: ");
+        double num = scanner.nextDouble();
+        System.out.printf("O valor digitado foi %.2f e sua porção inteira é %.0f.\n", num, Math.floor(num));
+        scanner.close();
     }
 }
